6da420b7b15356ebbe98d2b44a1e4f600f926d65
Sergey Kovalev <w3af@kovalev.com.ru>
2015-03-19 12:56:24 +0300

    Current

--- tests.py
+++ tests.py
@@ -1,10 +1,6 @@
-#!/usr/bin/env python
-# encoding: utf-8
+"""Tests for w3af_batch."""
 
-import logging
 import unittest
-import threading as th
-from StringIO import StringIO
 from mock import patch
 from mock import Mock
 
@@ -14,53 +10,41 @@ from w3af_batch import ScanExecutor
 from w3af_batch import STOPSCAN
 
 
-class w3afCoreMock(Mock):
+class W3afCoreMock(Mock):
+    """w3afCore mock."""
     pass
 
 
-@patch('w3af.core.controllers.w3afCore.w3afCore', new=w3afCoreMock())
+@patch('w3af.core.controllers.w3afCore.w3afCore', new=W3afCoreMock())
 class ScanExecutorTest(unittest.TestCase):
+    """Tests for ScanExecutor class."""
     target = 'target'
     profile = 'profile'
     timeout = 1
 
     def setUp(self):
-        self.log = StringIO()
-        logging.basicConfig(stream=self.log,
-                            level=logging.DEBUG,
-                            format='%(message)s')
-        self.thread = th.Thread(target=ScanExecutor(self.target,
-                                                    self.profile,
-                                                    self.timeout))
+        self.thread = ScanExecutor(self.target,
+                                   self.profile,
+                                   self.timeout)
         self.thread.start()
 
     def test_child_started(self):
-        self.assertEqual(self.log.getvalue(),
-                         'Scan started: %s\n' % self.target)
+        """Check if child was started."""
+        self.assertTrue(self.thread.done or self.thread.worker.is_alive())
                          
 
     def tearDown(self):
         self.thread.join()
 
 
-class LoggerTest(unittest.TestCase):
-    def setUp(self):
-        self.log = StringIO()
-        self.logger = logging.getLogger(__name__)
-        logging.basicConfig(stream=self.log,
-                            level=logging.DEBUG,
-                            format='%(message)s')
-
-    def test_stream_handler(self):
-        self.logger.debug('Test')
-        self.assertEqual('Test\n', self.log.getvalue())
-
-
 @patch('w3af.core.controllers.w3afCore.w3afCore')
-class w3afBatchTest(unittest.TestCase):
-    def test_execute_scan_framework_exception(self, w3af):
+class W3afBatchTest(unittest.TestCase):
+    """Test for function bases w3af_batch."""
+    def test_framework_exception(self, w3af):
+        """Test if exception is raised in execute_scan."""
         w3af.side_effect = BaseFrameworkException('w3af error')
         try:
             execute_scan('target', 'profile', 1)
         except BaseFrameworkException:
             pass
+        self.assertTrue(True)
